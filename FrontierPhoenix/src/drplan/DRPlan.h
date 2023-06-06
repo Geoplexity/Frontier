a22bb3c16a5d98238542097ebef4064bdb29652a
insert-username <insert-username@users.noreply.github.com>
2023-06-06 17:27:20 +0200

    Started DRPlan.

---
+++ FrontierPhoenix/src/drplan/DRPlan.h
@@ -0,0 +1,18 @@
+#ifndef FRONTIER_PHOENIX_DRPLAN_DRPLAN_H
+#define FRONTIER_PHOENIX_DRPLAN_DRPLAN_H
+
+namespace ffnx::drplan {
+
+    /**
+     * Operates on a flowgraph. Output should be a list of
+     * FlowGraphCommands, indicating the required mutations
+     * to the graph.
+     */
+    class DRPlan {
+
+
+    };
+
+}
+
+#endif
---
+++ FrontierPhoenix/src/drplan/README.md
@@ -0,0 +1,3 @@
+# Decomposition-Recomposition Plan
+
+This package provides an API for mutating flowgraphs based on a decomposition/recomposition plan algorithm.
